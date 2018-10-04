6738c0ade6ce71015250288386c6edf19cb61168
OPersian <olena.persianova@gmail.com>
2018-10-04 23:45:09 +0300

    Add main logic for knight moves task

--- src/com/company/KnightMovesChess.java
+++ src/com/company/KnightMovesChess.java
@@ -7,8 +7,11 @@
 
 public class KnightMovesChess {
 
-    public static boolean checkIfCanMove(int a, int b, int a1, int b1) {
-        // 4 corner cases
+    public static boolean checkIfCanMove(int x, int y, int x1, int y1) {
+
+        // We don't care about the field
+
+        // 4 corner cases (don't care either)
         // [0, 0] --> [2, 1], [1, 2]
         // [7, 0] --> [5, 1], [7, 2]
         // [7, 7] --> [5, 7], [6, 5]
@@ -16,7 +19,16 @@ public class KnightMovesChess {
 
         boolean canMove = false;
 
-        // TODO put the logic here
+        if (y1 - y == 2 || y1 - y == -2) {
+            if (x1 - x == 1 || x1 - x == -1) {
+                canMove = true;
+            }
+
+        } else if (y1 - y == 1 || y1 - y == -1) {
+            if (x1 - x == 2 || x1 - x == -2) {
+                canMove = true;
+            }
+        }
 
         return canMove;
 
@@ -26,17 +38,26 @@ public class KnightMovesChess {
         int a, b, a1, b1;
 
         // TODO: take from args
-        a = 2;
-        b = 4;
+        // Success
+        a = 4;
+        b = 5;
+        a1 = 6;
+        b1 = 6;
+
+        // Failure
+        /*
+        a = 4;
+        b = 5;
         a1 = 6;
         b1 = 7;
+        */
 
         boolean canMove = checkIfCanMove(a, b, a1, b1);
         String desiredMove = "from {" +  a + ", " + b + "} to {" +  a1 + ", " + b1 + "}";
         if (canMove) {
             System.out.println("The knight can move " + desiredMove);
         } else {
-            System.out.println("The knight cannot move" + desiredMove);
+            System.out.println("The knight cannot move " + desiredMove);
 
         }
     }
