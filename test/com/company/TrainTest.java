799dce28f3098d75592b7338b845830b5fd42b51
olenalo <uleta@ukr.net>
2018-11-05 16:48:31 +0200

    Add .gitignore, add test folder

---
+++ test/com/company/TrainTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void desribeTrainScheme() {
+    }
+
+    @Test
+    void addCarriage() {
+    }
+
+    @Test
+    void addCarriages() {
+    }
+
+    @Test
+    void setItinerary() {
+    }
+}
