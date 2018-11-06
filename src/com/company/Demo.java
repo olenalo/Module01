58f520930260e0ac0fa30c1f25c7f3f9872dcc56
olenalo <uleta@ukr.net>
2018-11-06 02:51:24 +0200

    HW15: demo other cases

--- src/com/company/Demo.java
+++ src/com/company/Demo.java
@@ -4,6 +4,10 @@ public class Demo {
 
     public static void main(String[] args) {
         Train train = new Train(2, 1, 3);
+        // Train train = new Train(5, 0, 0);
+        // Train train = new Train(0, 2, 0);
+        // Train train = new Train(0, 0, 4);
+
         train.setItinerary("Kharkiv", "Kyiv");
 
         train.addCarriages();
